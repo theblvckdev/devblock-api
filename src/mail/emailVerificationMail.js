e32cb35a8d47e11fd303c7858e3dfa13c8798275
theblvckdev <nathancodes05@gmail.com>
2024-03-26 12:42:55 +0100

    send email verification function using nodemailer

---
+++ src/mail/emailVerificationMail.js
@@ -0,0 +1,46 @@
+const nodemailer = require('nodemailer');
+const path = require('path');
+const hbs = require('nodemailer-express-handlebars');
+
+const sendEmailVerificationMail = (options) => {
+  const { email, subject, verificationURL } = options;
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    logger: true,
+    secure: false,
+    debug: true,
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASSWORD,
+    },
+    ignoreTLS: true,
+  });
+
+  const source = path.join(__dirname, '../views/emailVerification.handlebars');
+  console.log(source);
+
+  transporter.use(
+    'compile',
+    hbs({
+      viewEngine: 'express-handlebars',
+      viewPath: './src/views/',
+    })
+  );
+
+  const mailOptions = {
+    from: 'Lumo <support@lumo.com>',
+    to: email,
+    subject,
+    template: 'emailVerification',
+    context: { layout: source, email, verificationURL },
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) console.log('ERROR 💣', err);
+    else console.log(`Email sent successfully ${info}`);
+  });
+};
+
+module.exports = sendEmailVerificationMail;

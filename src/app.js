2e271dc5fdc071cd8f6f4713718cbdfda8bb05af
theblvckdev <nathancodes05@gmail.com>
2024-03-26 22:56:17 +0100

    code update

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use('/api/user/', authRouter);
+app.use('/api/auth/', authRouter);
 
 // Handling unhandled endpoints
 app.all('*', (req, res, next) => {
